4a42ff980c91725562b5f913e0ccb81483bec921
juanmarvaldes25 <juan.valdesgonzalez@students.mq.edu.au>
2023-08-02 09:40:43 +1000

    Complete Task 3

--- src/Main.java
+++ src/Main.java
@@ -15,13 +15,31 @@ public class Main extends JFrame {
     private Main(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Canvas canvas = new Canvas();
+        this.setContentPane(canvas);
+        this.pack();
 this.setVisible(true);
 
     }
 
     class Canvas extends JPanel{
+        public Canvas(){
+            setPreferredSize(new Dimension(720, 720));
+        }
 
+        @Override
+        public void paint(Graphics g){
+            g.drawRect(10, 10, 700, 700);
+
+            for(int i = 10; i < 710; i+= 35){
+                for(int j = 10; j < 710; j+= 35){
+                    g.drawRect(i, j , 35, 35);
+                }
+            }
+        }
+ 
     }
+
+   
 
 }
 
