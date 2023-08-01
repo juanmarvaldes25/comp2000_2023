5441627f068cd79dfbc44a274b15d62610788e02
juanmarvaldes25 <juan.valdesgonzalez@students.mq.edu.au>
2023-08-02 09:13:38 +1000

    Start task 3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,27 @@
-public class Main {
+import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
+
+
+public class Main extends JFrame {
+
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Red vs. Blue");
+        //System.out.println("Red vs. Blue");
+        Main window = new Main();
+
     }
+
+    private Main(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Canvas canvas = new Canvas();
+this.setVisible(true);
+
+    }
+
+    class Canvas extends JPanel{
+
+    }
+
 }
 
